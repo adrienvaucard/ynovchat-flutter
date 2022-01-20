8101e1fa28ad23ff5ed76b17d99abf8d3d430330
Adrien <adrien.vaucard@gmail.com>
2022-01-20 14:29:45 +0100

    feat(messages): SEND Message

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
@@ -24,13 +25,14 @@ class _HomePageState extends State<HomePage> {
   late List<int> items;
   late StreamController<List<Message>> _streamControllerListMsgs;
   late Stream<List<Message>> _streamMsgs;
+  late TextEditingController tecMsg;
 
   @override
   void initState() {
     super.initState();
     initializeDateFormatting('fr_FR');
     setLocaleMessages("fr_short", FrShortMessages());
-    items = <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    tecMsg = TextEditingController();
     _streamControllerListMsgs = StreamController<List<Message>>();
     _streamMsgs = _streamControllerListMsgs.stream;
     fetchMessages();
@@ -42,32 +44,57 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      body: StreamBuilder<List<Message>>(
-        stream: _streamMsgs,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) return const Icon(Icons.error);
-          if (!snapshot.hasData) return const Center(child: CircularProgressIndicator());
-          return ListView.separated(
-              itemCount: snapshot.data!.length,
-              separatorBuilder: (BuildContext context, int index) => const Divider(thickness: 1.5),
-              itemBuilder: (context, index) =>
-                ListTile(
-                  leading: Image.network("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6e443f00-bb6b-41ea-ba1a-f0c52dfb7cdc/ddrhs33-df39d039-d9ec-4450-b3a6-5c125a38e1e7.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzZlNDQzZjAwLWJiNmItNDFlYS1iYTFhLWYwYzUyZGZiN2NkY1wvZGRyaHMzMy1kZjM5ZDAzOS1kOWVjLTQ0NTAtYjNhNi01YzEyNWEzOGUxZTcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.RnDoWNFkVfodcv1boGZ1lafSLMbTSwhe-37omwoGtA4"),
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(snapshot.data![index].author.username),
-                      Text(
-                        formatDateString(snapshot.data![index].created_at),
-                        style: TextStyle(fontStyle: FontStyle.italic)
-                      ),
-                    ],
+      body: Column(
+        children: [
+          Expanded(child: _buildList()),
+          Row(children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: tecMsg,
+                  decoration: const InputDecoration(
+                    hintText: "Enter your message"
                   ),
-                  subtitle: Text(snapshot.data![index].content),
-                )
-          );
-        }
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => _sendMessage(context),
+              child: const Icon(Icons.send),
+            )
+          ],)
+        ],
       )
+    );
+  }
+
+  StreamBuilder<List<Message>> _buildList() {
+    return StreamBuilder<List<Message>>(
+      stream: _streamMsgs,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) return const Icon(Icons.error);
+        if (!snapshot.hasData) return const Center(child: CircularProgressIndicator());
+        return ListView.separated(
+            itemCount: snapshot.data!.length,
+            separatorBuilder: (BuildContext context, int index) => const Divider(thickness: 1.5),
+            itemBuilder: (context, index) =>
+              ListTile(
+                //leading: Image.network("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6e443f00-bb6b-41ea-ba1a-f0c52dfb7cdc/ddrhs33-df39d039-d9ec-4450-b3a6-5c125a38e1e7.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzZlNDQzZjAwLWJiNmItNDFlYS1iYTFhLWYwYzUyZGZiN2NkY1wvZGRyaHMzMy1kZjM5ZDAzOS1kOWVjLTQ0NTAtYjNhNi01YzEyNWEzOGUxZTcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.RnDoWNFkVfodcv1boGZ1lafSLMbTSwhe-37omwoGtA4"),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(snapshot.data![index].author.username),
+                    Text(
+                      formatDateString(snapshot.data![index].created_at),
+                      style: TextStyle(fontStyle: FontStyle.italic)
+                    ),
+                  ],
+                ),
+                subtitle: Text(snapshot.data![index].content),
+              )
+        );
+      }
     );
   }
 
@@ -95,6 +122,30 @@ class _HomePageState extends State<HomePage> {
           }
         }, 
         onError: (_, err) => log("Error while fetching messages"));
+  }
+
+  void _sendMessage (BuildContext context) {
+    Future<Response> res = post(
+        Uri.parse("https://flutter-learning.mooo.com/messages/"),
+        headers: { 'Authorization':"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjQyNjcxMzMyLCJleHAiOjE2NDUyNjMzMzJ9.46AmdmoaNWPaYdDoR-4YImCSBNROendkxWD5_oz39Nc"},
+        body: {
+          "content": tecMsg.text,
+        });
+
+    res.then((value) {
+      if (value.statusCode == 200) {
+        Map<String,dynamic> bodyJson = jsonDecode(value.body);
+
+        SnackBar snackbar = SnackBar(
+            content: Text("Message Sent")
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackbar);
+      }
+    }, onError: (obj) {
+      log("Send Error : " + obj.toString());
+    });
+
+    tecMsg.text = "";
   }
 
   @override
